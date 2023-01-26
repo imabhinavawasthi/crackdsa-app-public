e2dce60277bc8e29098e56cef24fcbeb2df48283
Abhinav Awasthi <awasthiabhinav744@gmail.com>
2023-01-26 15:07:08 +0530

    done 2

--- src/data.js
+++ src/data.js
@@ -121,7 +121,6 @@ export const footerData = {
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
   ],
   footNote: `
-    <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 float-left rounded-sm bg-[url(https://onwidget.com/favicon/favicon-32x32.png)]"></span>
-    Made by <a class="text-blue-600 hover:underline dark:text-gray-200" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+    Made with ❤️ by <a class="text-blue-600 hover:underline dark:text-gray-200" href="https://crackdsa.com/"> crackDSA</a> · All rights reserved.
   `,
 };
